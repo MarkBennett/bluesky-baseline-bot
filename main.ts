127cf96311a1fdaa7e7199534057d2386db62ea8
Mark Bennett <mark@burmis.ca>
2025-06-16 14:27:06 -0600

    Parse the data.json and extract new features.

--- main.ts
+++ main.ts
@@ -15,8 +15,7 @@ type BrowserKey =
   | "firefox_android"
   | "safari"
   | "safari_ios";
-type FeatureStatus = "available";
-
+type FeatureStage = "newly" | "widely";
 interface Feature {
 
 }
@@ -87,10 +86,44 @@ async function getBlueskyAgent(): Promise<AtpAgent> {
   return agent;
 }
 
-const NEWLY_AVAILABLE_FEATURES_URL = "https://web-platform-dx.github.io/web-features-explorer/newly-available.xml";
-const NEW_WIDELY_AVAILABLE_FEATURES_URL = "https://web-platform-dx.github.io/web-features-explorer/widely-available.xml";
+const WEB_PLATFORM_REPO = "web-platform-dx/web-features";
 
-async function hashFeature(feature: any): string {
+async function getLatestWebPlatformReleaseData() {
+  // Use the GitHub API to get the latest release data
+  const response = await fetch(
+    `https://api.github.com/repos/${WEB_PLATFORM_REPO}/releases/latest`,
+    {
+      headers: {
+        "Accept": "application/vnd.github.v3+json",
+        "User-Agent": "web-features-bot",
+      },
+    },
+  );
+  if (!response.ok) {
+    throw new Error(`Failed to fetch latest release data: ${response.statusText}`);
+  }
+  const releaseData = await response.json();
+
+  // Check the release assets for the url of the "data.json" file
+  const dataAsset = releaseData.assets.find((asset: any) => asset.name === "data.json");
+  if (!dataAsset) {
+    throw new Error("No data.json asset found in the latest release");
+  }
+  const dataUrl = dataAsset.browser_download_url;
+  if (!dataUrl) {
+    throw new Error("No download URL found for data.json asset");
+  }
+
+  const dataResponse = await fetch(dataUrl);
+  if (!dataResponse.ok) {
+    throw new Error(`Failed to fetch data.json: ${dataResponse.statusText}`);
+  }
+  const latestData = await dataResponse.json();
+
+  return latestData;
+}
+
+async function hashFeature(feature: any): Promise<string> {
   const featureString = JSON.stringify(feature);
   const featureStringUtf8 = new TextEncoder().encode(featureString);
 
@@ -102,7 +135,7 @@ async function hashFeature(feature: any): string {
 }
 
 async function getFeatureHashFromDB(featureId: string): Promise<string | null> {
-  const existingHash = await kv.get<string>(["features", featureId]);
+  const existingHash = await kv.get<string>([`features`, featureId]);
   if (existingHash) {
     return existingHash.value;
   }
@@ -113,37 +146,29 @@ async function setFeatureHashInDB(featureId: string, hash: string): Promise<void
   await kv.set(["features", featureId], hash);
 }
 
-async function getNewFeaturesFromRSS() {
-  // Retrieve the RSS feed for newly available features
-  const parser = new Parser();
-  const newlyAvailableFeaturesFeed = await parser.parseURL(NEWLY_AVAILABLE_FEATURES_URL);
+async function extractNewFeaturesFromData(): Promise<Feature[]> {
+  // Retrieve the newest web-features data
+  const latestData = await getLatestWebPlatformReleaseData();
 
-  // For each item in the feed, compare a hash of the item to one stored in the Deno.KV store
+  // For each item in the data, compare a hash of the item to one stored in the Deno.KV store
   // If the hash is not found, publish the feature to Bluesky and store the hash in Deno.KV
-  const newlyAvailableFeatures = newlyAvailableFeaturesFeed.items || [];
-  const newNewlyAvailableFeatures: Feature[] = [];
-  for (const item of newlyAvailableFeatures) {
-    const featureId = item.title;
-
-    if (!featureId) {
-      console.warn("Feature ID is missing in item:", item);
-      continue;
-    }
-
+  const featureItems = latestData.features;
+  const newFeatureItems: Feature[] = [];
+  for (const featureKey of Object.keys(featureItems)) {
+    const item = featureItems[featureKey];
     const featureHash = await hashFeature(item);
 
-    const existingHash = await getFeatureHashFromDB(featureId);
+    const existingHash = await getFeatureHashFromDB(featureKey);
     if (existingHash === null || existingHash !== featureHash) {
-      newNewlyAvailableFeatures.push(item);
-      await setFeatureHashInDB(featureId, featureHash);
+      newFeatureItems.push(item);
+      await setFeatureHashInDB(featureKey, featureHash);
     }
   }
 
-  return { newNewlyAvailableFeatures };
+  return newFeatureItems;
 }
-
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  const { newNewlyAvailableFeatures, newWidelyAvailableFeatures } = await getNewFeaturesFromRSS();
+  const newFeatures = await extractNewFeaturesFromData();
 }
